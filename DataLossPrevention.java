e01a1b0ff3febc971b6c9b6f1396305ab99dc3ae
LHenry-Mitchell <54952288+LHenry-Mitchell@users.noreply.github.com>
2019-09-17 09:43:40 -0700

    Add files via upload

---
+++ DataLossPrevention.java
@@ -0,0 +1,17 @@
+/******************
+* DataLossPrevention.java
+* Description: prints three different strategies for small companies to prevent
+* data loss
+* Lu Henry-Mitchell, period 1
+* 9-16-19
+******************/
+
+public class DataLossPrevention
+{
+    public static void main (String [] args)
+    {
+        System.out.println("One strategy is to educate employees about data security.");
+        System.out.println("Another strategy is to encrypt data.");
+        System.out.println("Small companies can also use strong password protection.");
+    }
+}
